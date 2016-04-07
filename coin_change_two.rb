adedcc8398f414e42442985865c9ebfc418d038b
jennifer Grudi <jen.grudi@gmail.com>
2016-04-07 19:56:22 -0400

    test_four_coins_due

--- coin_change_two.rb
+++ coin_change_two.rb
@@ -1,3 +1,3 @@
-def change_maker(coins)
-	result = {"quarter" => 0, "dime" => 0, "nickel" => 0, "penny" => 0}
+def change_maker(amount)
+	result = {"quarter" => 0, "dime" => 0, "nickel" => 0, "penny" => amount}
 end	
--- test_coin_change_two.rb
+++ test_coin_change_two.rb
@@ -2,9 +2,13 @@ require "minitest/autorun"
 require_relative "coin_change_two.rb"
 
 class TestCoinChange <Minitest::Test
-	def test_change_is_0
+	def test_change_due_is_0
 		assert_equal({"quarter" => 0, "dime" => 0, "nickel" => 0, "penny" => 0}, change_maker(0))
-	end		
+	end
+
+	def test_change_due_is_4
+		assert_equal({"quarter" => 0, "dime" => 0, "nickel" => 0, "penny" => 4}, change_maker(4))
+	end				
 
 
 

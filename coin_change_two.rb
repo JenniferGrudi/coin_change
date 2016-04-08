0a6349bd95de110c6eaa8cc1be7d05fb0e01e493
jennifer Grudi <jen.grudi@gmail.com>
2016-04-08 16:47:31 -0400

    passing_test_change_due_12

--- coin_change_two.rb
+++ coin_change_two.rb
@@ -1,19 +1,17 @@
 def change_maker(amount)
  result = {"quarter" => 0, "dime" => 0, "nickel" => 0, "penny" => amount}
 
- 	if amount >= 5
+ 	if amount >= 10
+     result["dime"] = 1
+ 	 amount = amount - 10
+ 	 result["penny"] = amount
+
+ 	elsif amount >= 5
  	 result["nickel"] = 1
- 	 result["penny"] = amount - 5
- 	else
- 	 amount < 5
+ 	 amount = amount - 5
  	 result["penny"] = amount 
  	end
 
- 	if amount >= 10
-     result["dime"] = 1
- 	 result["nickel"] = amount - 10
- 	 result["penny"] = amount - 10
- 	end	
-
   result
 end	
+
